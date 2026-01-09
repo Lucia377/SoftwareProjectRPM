01feb82f60bd38d48bdb22e8c8de3f17d262ca40
Nish Pahuja <nishtha.pahuja22@imperial.ac.uk>
2026-01-09 14:20:14 +0000

    Added code in main file generating random values for vital signs

--- RPM_monitor/src/main/java/Main.java
+++ RPM_monitor/src/main/java/Main.java
@@ -2,6 +2,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        //code to test if the generator works
+        VitalSignsGenerator signGenerator = new  VitalSignsGenerator();
+        System.out.println("Heart Rate: " + signGenerator.generateHeartRate() + " bpm");
+        System.out.println("Blood Pressure: " + signGenerator.generateBloodPressure());
+        System.out.println("ECG: " + signGenerator.generateECG() + " mV");
+        System.out.println("Respiratory Rate: " + signGenerator.generateRespiratoryRate() + " breaths/min");
+        System.out.println("Body Temperature: " + signGenerator.generateBodyTemperature() + " °C");
+
     }
+
+
+
 
 }
